17dabc53055ee1393ed8b7c172447a9aebb0a511
MahirAbsar <iamagamer1998@gmail.com>
2026-06-19 10:51:56 +0600

    feat(): initiate prisma, seed db

---
+++ prisma/seed.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import sampleData from "@/db/sample-data";
+
+async function main() {
+  const prisma = new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    }),
+  });
+  await prisma.product.deleteMany();
+
+  await prisma.product.createMany({ data: sampleData.products });
+
+  console.log("Database seeded successfully");
+}
+
+main();
